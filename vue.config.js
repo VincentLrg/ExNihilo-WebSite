804fa8d615f068d6368841d6d423d7d00da8c299
VincentLrg_ <lrgl.vincent@gmail.com>
2020-05-12 16:17:26 +0200

    Delete fonts

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,32 @@
 module.exports = {
-    css: {
-      loaderOptions: {
-        sass: {
-            prependData: `
-              @import "src/assets/scss/app.scss";
-              @import "src/assets/scss/content.scss";
-              @import "src/assets/scss/fonts.scss";
-              @import "src/assets/scss/hero.scss";
-              @import "src/assets/scss/variables.scss";
-            `
-          }
-      }
-    },
-    configureWebpack: {
-        module: {
-          rules: [{
-            test: /\.(ttf|otf|eot|woff|woff2)$/,
-            use: {
-              loader: "file-loader",
-              options: {
-                name: "src/assets/fonts/[name].[ext]",
-              },
-            },
-          }]
+  css: {
+    loaderOptions: {
+      sass: {
+          prependData: `
+            @import "src/assets/scss/app.scss";
+            @import "src/assets/scss/content.scss";
+            @import "src/assets/scss/cursor.scss";
+            @import "src/assets/scss/fonts.scss";
+            @import "src/assets/scss/glitchV1.scss";
+            @import "src/assets/scss/glitchV2.scss";
+            @import "src/assets/scss/hero.scss";
+            @import "src/assets/scss/teams.scss";
+            @import "src/assets/scss/variables.scss";
+          `
         }
+    }
+  },
+  configureWebpack: {
+      module: {
+        rules: [{
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "src/assets/fonts/[name].[ext]",
+            },
+          },
+        }]
       }
-  };
+    }
+};
