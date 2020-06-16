3435742e6fcf55a272172b4381e4778b82b4ec70
VincentLrg_ <lrgl.vincent@gmail.com>
2020-06-16 13:43:37 +0200

    DL button fix

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -98,6 +98,10 @@ export default {
                     e.preventDefault()
                     alert('Le jeu sera disponible au téléchargement le 18 juin !')
                 })
+                document.querySelector('.exn_content_promo .col1 a.btn').addEventListener('touchstart', (e)=>{
+                    e.preventDefault()
+                    alert('Le jeu sera disponible au téléchargement le 18 juin !')
+                })
                 break
         }
         if(this.index == 1){
